3a2c7c9e9cfd03e772e350784f5dd860ca9c23fe
Pratik Kumbhar <142420903+Pratik5357@users.noreply.github.com>
2025-12-28 20:03:55 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -73,18 +73,17 @@ export const loginUser = async (req, res) => {
 
 export const refreshToken = (req, res) => {
   const token = req.cookies.refreshToken;
-  if (!token) return res.sendStatus(401);
+  if (!token) return res.status(401).json({ message: "No refresh token" });
 
   jwt.verify(token, process.env.JWT_REFRESH_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.status(401).json({ message: "Invalid refresh token" });
 
     const newAccessToken = jwt.sign(
-      { id: decoded.id },
+      { id: decoded.id, role: decoded.role },
       process.env.JWT_SECRET,
       { expiresIn: "15m" }
     );
 
     res.json({ accessToken: newAccessToken });
   });
-
 };
